aa974ea57cf90203537e4a9a8e475c41961793a4
quintin.tuck <tuckquintin@hotmail.com>
2023-11-22 15:45:37 -0330

    Album solution started. Fetch seems to work in console

--- Albums/albums.js
+++ Albums/albums.js
@@ -1,7 +1,19 @@
 $(document).ready(() => {
 
-   fetch("jsonplaceholder.typicode.com")
-   // do the response and stuff here
+  getAlbums(1);
 
 });
 
+async function getUsers() {
+  const response = await fetch("https://jsonplaceholder.typicode.com/users");
+  const users = await response.json();
+  console.log(users);
+}
+
+async function getAlbums(UserID) {
+  let link = "https://jsonplaceholder.typicode.com";
+  link += "/posts?userId=" + UserID;
+  const response = await fetch(link);
+  const posts = await response.json();
+  console.log(posts);
+}
